fcfdf0d780c2812417897c29ef70c8ad763a2dbd
Adi17026 <adich170206@gmail.com>
2026-04-09 22:10:24 +0530

    First commit

---
+++ emote.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main() 
+{ long long n,m,k;
+    cin>>n>>m>>k;
+    vector<int>nums(n);
+    for (int i = 0; i < nums.size(); i++)
+    {
+        cin>>nums[i];
+    }
+    sort(nums.begin(),nums.end());
+    long long m1=nums[n-1];
+    long long m2=nums[n-2];
+    long long possible_cycle=m/(k+1);
+    long long ans=possible_cycle*(m1*k+m2);
+    cout<<ans<<endl;
+    
+  return 0;
+}
--- hello_world.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main() 
-{ cout<<"hELLO World";
-  return 0;
-}
